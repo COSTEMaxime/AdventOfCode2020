5178d926ae73ed5aeea1431afe94728c3b163b89
COSTE MAXIME <maxime.coste@viacesi.fr>
2021-03-10 23:21:02 +0100

    Added Day 8

---
+++ Day_08/GameConsole.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    class GameConsole
+    {
+        private int Pc;
+        public int Acc { get; private set; }
+        public int Status { get; private set; }
+        private Instruction[] Instructions;
+        private Instruction[] BackupInstructions;
+
+        private int InstructionSwapCounter;
+
+        public GameConsole(string[] rawOpcodes)
+        {
+            Pc = 0;
+            Acc = 0;
+            Status = 0;
+            InstructionSwapCounter = 0;
+
+            LoadProgram(rawOpcodes);
+        }
+
+        private void LoadProgram(string[] rawOpcodes)
+        {
+            Instructions = new Instruction[rawOpcodes.Length];
+            BackupInstructions = new Instruction[rawOpcodes.Length];
+
+            for (int i = 0; i < rawOpcodes.Length; i++)
+            {
+                string[] tokens = rawOpcodes[i].Split(' ');
+                Instructions[i] = new Instruction(tokens[0], int.Parse(tokens[1]));
+                BackupInstructions[i] = new Instruction(tokens[0], int.Parse(tokens[1]));
+            }
+        }
+
+        public void Reset()
+        {
+            for (int i = 0; i < BackupInstructions.Length; i++)
+            {
+                Instructions[i] = BackupInstructions[i];
+            }
+
+            Pc = 0;
+            Acc = 0;
+            Status = 0;
+        }
+
+        public void Run()
+        {
+            Instruction nextInstruction = Instructions[Pc];
+            while (nextInstruction.AlreadyExecuted == false)
+            {
+                Instructions[Pc].AlreadyExecuted = true;
+                switch (nextInstruction.Opcode)
+                {
+                    case "acc":
+                        Acc += nextInstruction.Argument;
+                        Pc++;
+                        break;
+                    case "jmp":
+                        Pc += nextInstruction.Argument;
+                        break;
+                    case "nop":
+                        Pc++;
+                        break;
+                }
+
+                if (Pc >= Instructions.Length) {
+                    Status = -1;
+                    return;
+                }
+                else if (Pc < 0)
+                {
+                    return;
+                }
+
+                nextInstruction = Instructions[Pc];
+            }
+        }
+
+        public void SwapNextInstruction()
+        {
+            if (Instructions[InstructionSwapCounter].Opcode == "nop")
+            {
+                Instructions[InstructionSwapCounter].Opcode = "jmp";
+                InstructionSwapCounter++;
+            }
+            else if (Instructions[InstructionSwapCounter].Opcode == "jmp")
+            {
+                Instructions[InstructionSwapCounter].Opcode = "nop";
+                InstructionSwapCounter++;
+            }
+            else
+            {
+                InstructionSwapCounter++;
+                SwapNextInstruction();
+            }
+        }
+    }
+
+    public struct Instruction
+    {
+        public string Opcode { get; set; }
+        public int Argument { get; }
+        public bool AlreadyExecuted { get; set; }
+
+        public Instruction(string opcode, int argument)
+        {
+            this.Opcode = opcode;
+            this.Argument = argument;
+            AlreadyExecuted = false;
+        }
+    }
+}
---
+++ Day_08/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] content = System.IO.File.ReadAllLines(@"input.txt");
+
+            GameConsole console = new GameConsole(content);
+
+            Console.WriteLine("Puzzle 1 : " + Puzzle1(console));
+            Console.WriteLine("Puzzle 2 : " + Puzzle2(console));
+        }
+
+        static int Puzzle1(GameConsole console)
+        {
+            console.Run();
+            return console.Acc;
+        }
+
+        static int Puzzle2(GameConsole console)
+        {
+            while (console.Status != -1)
+            {
+                console.Reset();
+                console.SwapNextInstruction();
+                console.Run();
+            }
+
+            return console.Acc;
+        }
+    }
+}
