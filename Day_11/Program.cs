9cb64a056d02e4f2867d4cae2fae5dad30e0a482
COSTE MAXIME <maxime.coste@viacesi.fr>
2021-03-14 19:59:01 +0100

    Added Day 11

---
+++ Day_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] content = System.IO.File.ReadAllLines(@"input.txt");
+
+            SeatLayoutSolver solver = new SeatLayoutSolver(content);
+
+            Console.WriteLine("Puzzle 1 : " + Puzzle1(solver));
+            Console.WriteLine("Puzzle 2 : " + Puzzle2(solver));
+        }
+
+        static int Puzzle1(SeatLayoutSolver solver)
+        {
+            solver.Puzzle1Setup();
+            solver.Solve();
+            return solver.GetOccupiedSeatsCount();
+        }
+
+        static int Puzzle2(SeatLayoutSolver solver)
+        {
+            solver.Reset();
+            solver.Puzzle2Setup();
+            solver.Solve();
+            return solver.GetOccupiedSeatsCount();
+        }
+    }
+}
---
+++ Day_11/SeatLayoutSolver.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11
+{
+    delegate int SeatCountBehaviour(int i, int j);
+
+    class SeatLayoutSolver
+    {
+        private string[] data;
+        private char[,] layout;
+        private int occupiedSeatsThreeshold;
+        private SeatCountBehaviour seatCountBehaviour;
+
+        public SeatLayoutSolver(string[] data)
+        {
+            this.data = data;
+            LoadLayout(data);
+        }
+
+        private void LoadLayout(string[] data)
+        {
+            layout = new char[data.Length, data[0].Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    layout[i, j] = data[i][j];
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            LoadLayout(data);
+        }
+
+        public void Puzzle1Setup()
+        {
+            occupiedSeatsThreeshold = 4;
+            seatCountBehaviour = new SeatCountBehaviour(GetOccupiedAdjacentSeatsCount);
+        }
+
+        public void Puzzle2Setup()
+        {
+            occupiedSeatsThreeshold = 5;
+            seatCountBehaviour = new SeatCountBehaviour(GetOccupiedLineSeatsCount);
+        }
+
+        public void Solve()
+        {
+            char[,] newLayout = (char[,])layout.Clone();
+            do
+            {
+                layout = (char[,])newLayout.Clone();
+                for (int i = 0; i < layout.GetLength(0); i++)
+                {
+                    for (int j = 0; j < layout.GetLength(1); j++)
+                    {
+                        int occupiedAdjacentSeatsCount = seatCountBehaviour(i, j);
+                        if (layout[i, j] == 'L' && occupiedAdjacentSeatsCount == 0)
+                        {
+                            newLayout[i, j] = '#';
+                        }
+                        else if (layout[i, j] == '#' && occupiedAdjacentSeatsCount >= occupiedSeatsThreeshold)
+                        {
+                            newLayout[i, j] = 'L';
+                        }
+                    }
+                }
+            }
+            while (
+            (
+                Enumerable.Range(0, layout.Rank).All(dimension => layout.GetLength(dimension) == newLayout.GetLength(dimension))
+                && layout.Cast<char>().SequenceEqual(newLayout.Cast<char>())
+            ) == false);
+        }
+
+        private int GetOccupiedAdjacentSeatsCount(int i, int j)
+        {
+            int occupiedSeatsCount = 0;
+
+            if (IsInbound(i - 1, j - 1) && IsSeatOccupied(i - 1, j - 1)) { occupiedSeatsCount++; }
+            if (IsInbound(i - 1, j + 0) && IsSeatOccupied(i - 1, j + 0)) { occupiedSeatsCount++; }
+            if (IsInbound(i - 1, j + 1) && IsSeatOccupied(i - 1, j + 1)) { occupiedSeatsCount++; }
+            if (IsInbound(i + 0, j - 1) && IsSeatOccupied(i + 0, j - 1)) { occupiedSeatsCount++; }
+            if (IsInbound(i + 0, j + 1) && IsSeatOccupied(i + 0, j + 1)) { occupiedSeatsCount++; }
+            if (IsInbound(i + 1, j - 1) && IsSeatOccupied(i + 1, j - 1)) { occupiedSeatsCount++; }
+            if (IsInbound(i + 1, j + 0) && IsSeatOccupied(i + 1, j + 0)) { occupiedSeatsCount++; }
+            if (IsInbound(i + 1, j + 1) && IsSeatOccupied(i + 1, j + 1)) { occupiedSeatsCount++; }
+
+            return occupiedSeatsCount;
+        }
+
+        private int GetOccupiedLineSeatsCount(int i, int j)
+        {
+            int occupiedSeatsCount = 0;
+
+            if (LineHasOccupiedSeat(i, j, -1, -1)) { occupiedSeatsCount++; }
+            if (LineHasOccupiedSeat(i, j, -1, +0)) { occupiedSeatsCount++; }
+            if (LineHasOccupiedSeat(i, j, -1, +1)) { occupiedSeatsCount++; }
+            if (LineHasOccupiedSeat(i, j, +0, -1)) { occupiedSeatsCount++; }
+            if (LineHasOccupiedSeat(i, j, +0, +1)) { occupiedSeatsCount++; }
+            if (LineHasOccupiedSeat(i, j, +1, -1)) { occupiedSeatsCount++; }
+            if (LineHasOccupiedSeat(i, j, +1, +0)) { occupiedSeatsCount++; }
+            if (LineHasOccupiedSeat(i, j, +1, +1)) { occupiedSeatsCount++; }
+
+            return occupiedSeatsCount;
+        }
+
+        private bool LineHasOccupiedSeat(int i, int j, int iStep, int jStep)
+        {
+            int currentI = i + iStep;
+            int currentJ = j + jStep;
+            while (IsInbound(currentI, currentJ))
+            {
+                if (IsSeatNotEmpty(currentI, currentJ)) { return IsSeatOccupied(currentI, currentJ); }
+                currentI += iStep;
+                currentJ += jStep;
+            }
+
+            return false;
+        }
+
+        private bool IsInbound(int i, int j)
+        {
+            return (i >= 0 && j >= 0 && i < layout.GetLength(0) && j < layout.GetLength(1));
+        }
+
+        private bool IsSeatOccupied(int i, int j)
+        {
+            return layout[i, j] == '#';
+        }
+
+        private bool IsSeatNotEmpty(int i, int j)
+        {
+            return layout[i, j] != '.';
+        }
+
+        public int GetOccupiedSeatsCount()
+        {
+            int occupiedSeatsCount = 0;
+            foreach (var element in layout)
+            {
+                if (element == '#')
+                {
+                    occupiedSeatsCount++;
+                }
+            }
+
+            return occupiedSeatsCount;
+        }
+    }
+}
